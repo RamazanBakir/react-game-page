479add5e91491fd7d322b0360e26013b456f9195
yemicodes <yemicodes@gmail.com>
2021-03-16 02:34:02 +0300

    setting the mainlayout and the header..

---
+++ kidgame/src/component/header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
+
+const Header = () => {
+    return (
+        <>
+            <Navbar>
+                <LinkContainer to="/">
+                    <Navbar.Brand>
+                        Gefeasoft Kids Games '[ö_ö]'
+                    </Navbar.Brand>
+                </LinkContainer>
+            </Navbar>
+            <Nav>
+                <Nav.Item>
+                    <LinkContainer to="/">
+                        <Nav.Link>
+                            Home
+                        </Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                    <LinkContainer to="/games">
+                        <Nav.Link>
+                            Games
+                        </Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                    <LinkContainer to="/faq">
+                        <Nav.Link>
+                            Faq
+                        </Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                    <LinkContainer to="/contact">
+                        <Nav.Link>
+                            Contact Us
+                        </Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+            </Nav>
+        </>
+    )
+}
+
+export default Header;
--- kidgame/src/routes.js
+++ kidgame/src/routes.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import MainLayout from './hoc/mainLayout';
 import Home from './component/home';
+import Header from './component/header';
 
 const Routes = () => (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={Home}/>
-        </Switch>
+        <Header />
+        <MainLayout>
+            <Switch>
+                <Route path="/" component={Home}/>
+            </Switch>
+        </MainLayout>
+        
     </BrowserRouter>
 )
 
