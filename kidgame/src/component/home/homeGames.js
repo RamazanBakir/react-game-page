30978e6d3d63524386eb561be3987be6d2f7fda1
yemicodes <yemicodes@gmail.com>
2021-03-16 08:10:56 +0300

    json data showing on the homeGage page

--- kidgame/src/component/home/homeGames.js
+++ kidgame/src/component/home/homeGames.js
@@ -3,7 +3,9 @@ import { useSelector, useDispatch  } from 'react-redux';
 import { getGames } from '../../store/actions';
 import { Spinner, Button } from 'react-bootstrap';
 import Masonry from 'react-masonry-css';
+import Moment from 'react-moment';
 import { LinkContainer } from 'react-router-bootstrap';
+
 
 const HomeGames = () => {
     const homeGames = useSelector(state => state.games);
@@ -26,7 +28,7 @@ const HomeGames = () => {
     return (
         <>
             <Masonry 
-                breakpointCols={3}
+                breakpointCols={{default:3, 800: 2, 400: 1}}
                 className="my-masonry-grid"
                 columnClassName="my-masonry-grid_column"
             >
@@ -34,7 +36,34 @@ const HomeGames = () => {
                     homeGames.games ?
                         homeGames.games.map((item) => (
                             <div key={item.id}>
-                                a
+                                <img
+                                    style={{width:'100%',height:'400px'}}
+                                    src={item.image}
+                                />
+                                <div className="author">
+                                    <Moment format="DD MMMM">
+                                        {item.createdAt}
+                                    </Moment>
+                                </div>
+                                <div className="author">
+                                    <span>{item.author} -</span>  
+                                    
+                                </div>
+                                <div className="content">
+                                    <div className="title">{item.title}</div>
+                                    <div className="excerpt">{item.excerpt}</div>
+                                    <LinkContainer 
+                                        className="mt-3"
+                                        to={`/article/${item.id}`}
+                                    >
+                                    <Button 
+                                        variant="outline-dark"
+                                    >
+                                        View Game
+                                    </Button>
+                                    </LinkContainer>
+                                </div>
+                                
                             </div>
                         ))
                     :null
