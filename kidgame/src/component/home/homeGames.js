76a335a8ea87986c543bcd04f4eedb57ec66ec8f
yemicodes <yemicodes@gmail.com>
2021-03-16 07:27:50 +0300

    added mansory,
    i added mansory to view the list of the fake db i.e the json data

--- kidgame/src/component/home/homeGames.js
+++ kidgame/src/component/home/homeGames.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch  } from 'react-redux';
 import { getGames } from '../../store/actions';
 import { Spinner, Button } from 'react-bootstrap';
+import Masonry from 'react-masonry-css';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const HomeGames = () => {
-    const homeGame = useSelector(state => state.games);
+    const homeGames = useSelector(state => state.games);
     const dispatch = useDispatch();
     const [loading,setLoading] = useState(false);
 
@@ -14,15 +16,30 @@ const HomeGames = () => {
     },[dispatch])
 
     const loadMoreGames = () => {
-        const page = homeGame.page + 1;
+        const page = homeGames.page + 1;
         setLoading(true);
-        dispatch(getGames(homeGame,page,"desc",6)).then(() => {
+        dispatch(getGames(homeGames,page,"desc",6)).then(() => {
             setLoading(false);
         })
     }
     
     return (
         <>
+            <Masonry 
+                breakpointCols={3}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+            >
+                {
+                    homeGames.games ?
+                        homeGames.games.map((item) => (
+                            <div key={item.id}>
+                                a
+                            </div>
+                        ))
+                    :null
+                }
+            </Masonry>
             { loading ?
                 <div style={{ textAlign:'center'}}>
                     <Spinner animation="border" role="status">
@@ -30,7 +47,7 @@ const HomeGames = () => {
                     </Spinner>
                 </div>
             :null}
-            { !homeGame.end & !loading ? 
+            { !homeGames.end & !loading ? 
             <Button 
                 variant="outline-dark" 
                 onClick={() => loadMoreGames()}
